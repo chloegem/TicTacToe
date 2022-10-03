7b070f3f378aea9c22c6335e9525dee93346c63b
Chloe Gemayel <chloegemayel@Chloes-MacBook-Pro.local>
2022-10-03 20:17:11 +0300

    js part 1

---
+++ script.js
@@ -0,0 +1,19 @@
+const computer_player = 'X';
+const human = 'O';
+var table;
+const wins = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2]
+]
+
+const cells = document.querySelectorAll('.cell');
+startGameHandler();
+
+
+
