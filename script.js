2ceca8a3c740b4b128e2c8e8a2cf57bc452d3b2f
Chloe Gemayel <chloegemayel@Chloes-MacBook-Pro.local>
2022-10-03 20:18:15 +0300

    js part 2

--- script.js
+++ script.js
@@ -15,5 +15,13 @@ const wins = [
 const cells = document.querySelectorAll('.cell');
 startGameHandler();
 
-
+function startGameHandler(){
+	document.querySelector(".finishGame").style.display = "none";
+	table = Array.from(Array(9).keys());
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].innerText = '';
+		cells[i].style.removeProperty('background-color');
+		cells[i].addEventListener('click', turnClick, false);
+	}
+}
 
