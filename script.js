1ceb74ba2543380ac51f2102335b485bda475eee
Chloe Gemayel <chloegemayel@Chloes-MacBook-Pro.local>
2022-10-03 20:18:40 +0300

    js part 3

--- script.js
+++ script.js
@@ -25,3 +25,17 @@ function startGameHandler(){
 	}
 }
 
+function checkWinHandler(board, player){
+	let plays = board.reduce((a, e, i) =>
+		(e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+	for (let [index, win] of wins.entries()) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+	return gameWon;
+}
+
+
